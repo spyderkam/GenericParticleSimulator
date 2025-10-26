76aeb921d8d399654d9e6570b39c7cf4aeddbeb4
Kam <kamyarmojtahed@gmail.com>
2025-10-26 22:33:17 +0000

    Added `advance` with Euler method.
    Initiated Verlet integration. RK4 could be another one to add later.

--- src/pyparticlesim.py
+++ src/pyparticlesim.py
@@ -24,3 +24,19 @@ class Particle:
         self.mass = mass
         self.radius = radius
         self.force = np.array([0.0, 0.0])            # [fx, fy] accumulator
+
+    def advance(self, dt, method='euler'):
+        """Update position and velocity using accumulated forces."""
+        if method == 'euler':
+            self._advance_euler(dt)
+        elif method == 'verlet':
+            self._advance_verlet(dt)
+            # For Verlet, we'll need to store self.accel_prev (previous acceleration).
+            # We must add that attribute to __init__ and initialize it to zero.
+        else:
+            raise ValueError(f"Unknown integration method: {method}")
+    
+    def _advance_euler(self, dt):
+        acceleration = self.force / self.mass
+        self.vel += acceleration * dt
+        self.pos += self.vel * dt
