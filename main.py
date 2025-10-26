c0f2a7f99a18c22ef7f9c89f551dc3a23830d70e
Kam <kamyarmojtahed@gmail.com>
2025-10-26 23:45:47 +0000

    Moved *example usage* to main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from src.pyparticlesim import *
+
+# Example usage
+p = Particle(position=[0.0, 0.0], velocity=[1.0, 1.0], mass=1.0, radius=1.0)
+print(p.pos, p.vel)
+f1 = np.array([0.0, -9.8])  # Gravity
+f2 = np.array([1.0, 0.0])   # Constant force
+f = np.sum([f1, f2], axis=0)
+dt = 0.1
+p.apply_forces(dt, f1, f2)  # f vs. f1, f2
+print(p.pos, p.vel)
