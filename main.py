692574f1edee07c704311cd68fae6fee40824ebb
Kam <kamyarmojtahed@gmail.com>
2025-10-28 19:51:58 +0000

    Refactor particle parameters handling
    Refactor Particle initialization: Updated `Particle_Structure` to use particle parameters, improved error handling, and adjusted force calculations in `generic_central_force`.

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
-from src.pyparticlesim import Particle_Structure, Simulation
+from src.pyparticlesim import Particle, Particle_Structure, Simulation
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Before
-ps = Particle_Structure('circle', [0, 0, 1.0], 10)
-x0 = [p.pos[0] for p in ps.particles]
-y0 = [p.pos[1] for p in ps.particles]
+struct = Particle_Structure(structure='rectangle', init_points=[0, 0, 2.0, 1.0], nParticles=50)
+positions_0 = [particle.pos for particle in struct.particles]
 
-# Simulation run
-sim = Simulation(ps.particles, dt=0.01)
-def weight(p): return np.array([0.0, -9.81*p.mass])
-sim.run(100, weight)
+def weight(particle):
+    if not isinstance(particle, Particle):
+        raise ValueError("Input must be a Particle object")
+    return np.array([0.0, -9.81*particle.mass])
 
-# After
-x1 = [p.pos[0] for p in sim.particles]
-y1 = [p.pos[1] for p in sim.particles]
-
-# Plot
-plt.plot(x0, y0, 'bo', label='$t=0$')
-plt.legend()
-plt.axis('equal')
-#plt.savefig('before.pdf', bbox_inches='tight')
-
-plt.plot(x1, y1, 'ro', label=r'$t = 1\,\mathrm{s}$')
-plt.legend()
-plt.axis('equal')
-
-plt.savefig('before_after.pdf', bbox_inches='tight')
+plt.plot([p[0] for p in positions_0], [p[1] for p in positions_0], 'bo', label='$t=0$')
+plt.savefig('initial.pdf', bbox_inches='tight')
 plt.close()
+
+part0 = Particle(position=[0.0, 0.0], velocity=[5.0, 10.0], mass=1.0, radius=1.0)
+part1 = Particle(position=[2.0, 1.0], velocity=[20.0, 1.0], mass=1.0, radius=1.0)
+
+
+#print( (np.linalg.norm(part0.vel)*np.linalg.norm(part1.vel))/(np.linalg.norm(part0.pos - part1.pos)**2) )
+
+def generic_central_force(particle1: Particle, particle2: Particle, k=1.0):
+    """$$\pm k \frac{\left\vert v_1v_2\right\vert}{r^2} \hat{r}$$"""
+
+    if not isinstance(particle1, Particle) and not isinstance(particle2, Particle):
+         raise ValueError("At least one of the particles is NOT an instance of the Particle class")
+
+    distance_vector = particle1.pos - particle2.pos
+    distance = np.linalg.norm(distance_vector)
+    velocity_product = np.linalg.norm(particle1.vel)*np.linalg.norm(particle2.vel)
+
+    central_force = (k*velocity_product/distance**3)*distance_vector
+    return central_force
+
+print(generic_central_force(part0, part1))
