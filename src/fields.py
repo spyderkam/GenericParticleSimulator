c904c054b3c282cb3b95c4c908b2cb85e73c1ec4
Kam <kamyarmojtahed@gmail.com>
2025-11-02 17:34:58 +0000

    Created fields.py

---
+++ src/fields.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Kamyar Modjtahedzadeh"
+
+from pyparticlesim import Particle
+import numpy as np
+
+
+class SK_Field:
+    """
+    Stateless field class for computing particle-particle interaction forces.
+    
+    Computes N-body forces based on provided parameters. Supports multiple
+    force types simultaneously (gravity, Lennard-Jones, springs, etc.).
+    """
+
+    def __init__(self, **params):
+        """
+        Args:
+            **params: Force parameters (e.g., G, epsilon, sigma, k, cutoff)
+        """
+        
+        self.params = params
+
+    def compute_forces(self, particles):
+        """
+        Compute all pairwise forces for particle array.
+        
+        Args:
+            particles: Array of Particle objects
+            
+        Returns:
+            Array of force vectors [Fx, Fy] for each particle
+        """
+        
+        n = len(particles)
+        forces = np.zeros((n, 2))
+        
+        # Double loop over particle pairs
+        for i in range(n):
+            for j in range(i + 1, n):
+                # Compute pairwise force
+                f_ij = self._pairwise_force(particles[i], particles[j])
+                
+                # Newton's third law
+                forces[i] += f_ij
+                forces[j] -= f_ij
+        
+        return forces
+    
+    def _pairwise_force(self, p1, p2):
+        """
+        Compute force on p1 due to p2.
+        
+        Checks self.params to determine which forces to include.
+        """
+        
+        # Relative position
+        r_vec = p1.pos - p2.pos
+        r = np.linalg.norm(r_vec)
+        
+        if r == 0:
+            return np.array([0.0, 0.0])
+        
+        r_hat = r_vec / r
+        f_total = np.array([0.0, 0.0])
+        
+        if 'G' in self.params:
+            f_total += self._gravity(p1, p2, r, r_hat)
+        
+        if 'epsilon' in self.params and 'sigma' in self.params:
+            f_total += self._lennard_jones(p1, p2, r, r_hat)
+        
+        return f_total
+
+    def _gravity(self, p1, p2, r, r_hat):
+        """N-body gravitational force."""
+        G = self.params['G']
+        return -G * p1.mass * p2.mass / r**2 * r_hat
+
+    def _lennard_jones(self, p1, p2, r, r_hat):
+        """Lennard-Jones force."""
+        epsilon = self.params['epsilon']
+        sigma = self.params['sigma']
+        sigma_over_r = sigma / r
+        return 24 * epsilon * (2 * sigma_over_r**13 - sigma_over_r**7) / r * r_hat
