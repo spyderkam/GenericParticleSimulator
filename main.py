e5faaa27510c42e8880d653359cde1bcb8de2d17
Kam <kamyarmojtahed@gmail.com>
2025-11-03 00:40:19 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 part_rad = 1.0  # Particle radius
 
 # Create particle structure
-circle = pps.Particle_Structure('solid_circle', [0.0, 0.0, 1.0], 100, particle_radius=part_rad)
+circle = pps.Particle_Structure('solid_circle', [0.0, 0.0, 1.0], 1000, particle_radius=part_rad)
 positions = [particle.pos for particle in circle.particles]
 
 plt.plot([p[0] for p in positions], [p[1] for p in positions], 'bo', label='$t=0$')
@@ -17,20 +17,22 @@ plt.close()
 
 # Create field
 # ϵ (loosely) sets interaction strength and σ should match particle diameter
-field = pps.SK_Field(epsilon=1.0, sigma=part_rad*2)
+field = pps.SK_Field(epsilon=10000.0, sigma=part_rad*2)
+
+# Run parameters
+simulation_time = 0
+dt = 0.01
+n_steps = 100
 
 # Run simulation
-time = 0
-dt = 0.01
-
-for _ in range(100):
+for _ in range(n_steps):
     forces = field.compute_forces(circle.particles)
     for i, particle in enumerate(circle.particles):
         particle.apply_forces(dt, forces[i])
-    time += dt
+    simulation_time += dt
 
 # Check results
-print(f"Final time: {time}")
+print(f"Final time: {simulation_time}")
 #print(f"First particle position: {pps.ps.particles[0].pos}")
 
 final_positions = [particle.pos for particle in circle.particles]
@@ -38,6 +40,6 @@ final_positions = [particle.pos for particle in circle.particles]
 plt.plot([p[0] for p in final_positions], [p[1] for p in final_positions], 'bo', label='$t=0$')
 plt.axis('equal')
 plt.tight_layout()
-#plt.show()
+plt.show()
 plt.savefig('last_struc.pdf', bbox_inches='tight')
 plt.close()
