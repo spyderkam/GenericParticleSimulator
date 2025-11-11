323176fa9821424eb8170e9709e32d35ec1b4e61
Kam <kamyarmojtahed@gmail.com>
2025-11-11 21:32:30 +0000

    Simulation runtime: 673.4861 seconds
    ```python
    # Parameters
    G = 10.0                    # Tested with grav_softening=0.05, dt=1e-5
    grav_softening = 0.05       # Tested with G=10, dt=1e-5
    k_zeta = 10.0               # Make same as G
    zeta_softening = 0.05
    zeta_amplitude = 1          # Increased from 0.5
    omega_zeta = 3.33           # Decrease from G=10
    dt = 1e-5                   # Tested with G=10, grav_softening=0.05
    n_steps = 6000
    ```
    
    Ignoring fixed x limits to fulfill fixed data aspect with adjustable data limits.
    Simulation runtime: 673.4861 seconds
    Final time: 0.060

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ zeta_softening = 0.05
 zeta_amplitude = 1          # Increased from 0.5
 omega_zeta = 3.33           # Decrease from G=10
 dt = 1e-5                   # Tested with G=10, grav_softening=0.05
-n_steps = 4000
+n_steps = 6000
 
 # Record start time
 start_time = time.perf_counter()
